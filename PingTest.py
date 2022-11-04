e2bda56426803664a2b3c8342e4eebaa4be4279d
Tjugosex <mikael.skaremyr.te1@hotmail.com>
2022-11-04 10:22:45 +0100

    hej

---
+++ PingTest.py
@@ -0,0 +1,89 @@
+from pythonping import ping
+import time
+import smtplib, ssl
+from socket import gaierror
+from email.mime.text import MIMEText
+
+
+EMAIL = 'pingermikael@gmail.com'
+APP_PASSWORD = 'qrnlgclvotopvzfp'
+
+quitint = 0
+
+def main():
+
+    ############################################
+    #Open IP address file and separate ip addresses by comma
+    camIpDocument = open("IpAddresses.txt","r")
+    hostsList = str(camIpDocument.read())
+    hostsList = hostsList.rstrip()
+    #split string into list from commas
+    HOSTS = list(hostsList.split(","))
+    camIpDocument.close()
+
+    ############################################
+    #open cam names document this includes all the hostnames you want to pair with the ip addresses
+    nameDocument = open("Name.txt","r")
+    hostsNames = str(nameDocument.read())
+    hostsNames = hostsNames.rstrip()
+    #split cam names into list
+    cameraNames = list(hostsNames.split(","))
+    nameDocument.close()
+    ############################################
+    emailDocument = open("EmailAddresses.txt","r")
+    emailNames = str(emailDocument.read())
+    emailNames = emailNames.rstrip()
+    #split cam names into list
+    emailAddressList = list(emailNames.split(","))
+    emailDocument.close()
+    ############################################
+
+    #send cam names and IP addresses to ping function
+    Ping_Test(HOSTS,cameraNames,emailAddressList)
+def Ping_Test(hosts,camHostName,recipients):
+    #ping every ip address in the CamIpAddresses.txt file
+    i = 0
+    while i < len(hosts):
+        response = ping(hosts[i], verbose=True)
+        if 'Request timed out' in str(response):
+            #if request times out send email
+            Send_Email(hosts[i],camHostName[i],recipients)
+
+        time.sleep(5)
+        i+=1
+    
+
+def Send_Email(ip,name,receiver):
+    global quitint
+    quitint +=1
+    SUBJECT = "Server down"
+    TEXT = ("One of your servers is offline\nName: %s\nIP-address: %s\nB)" 
+        % ( name, ip))
+    msg = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(EMAIL, APP_PASSWORD)
+        server.sendmail(EMAIL, receiver, msg)
+        server.close()
+
+        print('Email sent!')
+    except Exception as e:
+        print(e)
+        print('Something went wrong...')
+    #define the SMTP server separately here:
+    
+def loop():
+    global quitint
+    main()
+    if quitint < 1:
+        print('Sleeping for 1/2 hour...')
+        time.sleep(3600)
+        loop()
+    if quitint > 1:
+        quitint = 0
+        print('Sleeping for a day...')
+        time.sleep(86400)
+        loop()
+        
+loop()
